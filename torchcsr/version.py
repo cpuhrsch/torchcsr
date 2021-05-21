f9a9a9f7991474ed4be956f4991aa6cc3f2e992d
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2021-05-20 17:42:18 -0700

    Some more CMake

--- torchcsr/version.py
+++ torchcsr/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1+7f45b05'
-git_version = '7f45b05940459648128abad8e939186aedf67b6e'
+__version__ = '0.0.1+6387896'
+git_version = '6387896aee96f4ae31f1c46c2244dac37d34858b'
 from torchcsr import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
